7869617e44d79d0c91279ea7bc36eeeb39fb4c09
Rachit Trivedi <114496132+technoprogramming92@users.noreply.github.com>
2025-03-11 18:45:57 +0530

    first commit

---
+++ public/assets/js/custom.js
@@ -0,0 +1,2 @@
+(function($){"use strict";$('.minus').click(function(){var $input=$(this).parent().find('.box');var count=parseInt($input.val())-1;count=count<1?1:count;$input.val(count);$input.change();return false;});$('.plus').click(function(){var $input=$(this).parent().find('.box');$input.val(parseInt($input.val())+1);$input.change();return false;});const tooltipTriggerList=document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList=[...tooltipTriggerList].map(tooltipTriggerEl=>new bootstrap.Tooltip(tooltipTriggerEl))})(jQuery);
